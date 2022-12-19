ebe78fca820435cb6dde2988e349ddd3b8d76f55
Your Name <you@example.com>
2022-12-20 12:05:05 +1300

    update loadstate

--- src/List/index.tsx
+++ src/List/index.tsx
@@ -1,25 +1,27 @@
 import { useEffect, useState } from "react";
 
+import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
 
 type Messages = {
-    cursor: Message[]
-    matches: number
+    cursor: Message[];
+    matches: number;
 }
 
 type Message = {
-    id: string,
-    messages: MessageBody[]
+    id: string;
+    messages: MessageBody[];
 }
 
 type MessageBody = {
-    body: string
-    date: string
+    body: string;
+    date: string;
 }
 
 const url = import.meta.env.VITE_API_URL;
 
 const List = () => {
+    const [loading, setLoading] = useState<boolean>(true);
     const [messages, setMessages] = useState<Messages>({
         cursor: [],
         matches: 0
@@ -40,17 +42,17 @@ const List = () => {
             if (resp.ok) {
                 const data = await resp.json()
                 setMessages(data)
+                setLoading(false)
             }
         }
         fetchList();
-    }, [])
-
-    console.log(messages)
+    }, []);
 
     return (
         <>
             <Typography variant="h3">Current chats</Typography>
-            {messages &&
+            {loading && <Skeleton height={200} />}
+            {!loading && messages &&
                 <>
                     {messages.cursor?.map((msg: Message, index: number) =>
                         <div key={index}>
